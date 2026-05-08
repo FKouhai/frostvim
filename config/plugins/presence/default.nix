4c9a467daf3d70c951c1347f7e3626c053bf2208
FKouhai <frandres00@gmail.com>
2026-05-08 22:19:32 +0200

    chore(presence): replace discord presence.nvim with cord.nvim

--- config/plugins/presence/default.nix
+++ config/plugins/presence/default.nix
@@ -12,10 +12,33 @@
   config = lib.mkMerge [
     { presence.enable = lib.mkDefault (!pkgs.stdenv.isDarwin); }
     (lib.mkIf config.presence.enable {
-      plugins.presence = {
+      plugins.cord = {
         enable = true;
         settings = {
-          neovim_image_text = "nvim";
+          editor = {
+            tooltip = "Neovim";
+          };
+          display = {
+            theme = "default";
+            flavor = "dark";
+          };
+          timestamp = {
+            enabled = true;
+            reset_on_idle = false;
+            reset_on_change = false;
+          };
+          idle = {
+            enabled = true;
+            timeout = 300000;
+            show_status = true;
+            smart_idle = true;
+          };
+          text = {
+            workspace.__raw = "function(opts) return 'In ' .. opts.workspace end";
+            editing.__raw = "function(opts) return 'Editing ' .. opts.filename end";
+            viewing.__raw = "function(opts) return 'Viewing ' .. opts.filename end";
+            dashboard = "Home";
+          };
         };
       };
     })
